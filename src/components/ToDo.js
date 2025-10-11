5c5c5a5e9f381cda52611d4b35f770e166a37823
sahir zakaria <sahir.alzakaria@gmail.com>
2025-10-11 14:47:03 +0300

    UI setup (work in progress)

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,18 +1,38 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
+import Grid from '@mui/material/GridLegacy';
+
+//Icons
+import IconButton from '@mui/material/IconButton';
+import CheckIcon from '@mui/icons-material/Check';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+
 
 export default function ToDo(){
     return (<>
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography variant='h2'>المهمة الأولى</Typography>
+    <Card sx={{ minWidth: 275 , background:"#0f1963ff" , color:"white" , marginTop:5 }}>
+      <CardContent >
+        <Grid container spacing={2} style={{padding:0 ,  margin:0 }} >
+          <Grid item xs={8} >
+              <Typography variant='h5' style={{textAlign:"right"}}>المهمة الأولى</Typography>
+          </Grid>
+          {/* Actions Buttons */}
+          <Grid  xs={4} display="flex" justifyContent="space-around" alignItems="center"> 
+            <IconButton aria-label="check" style={{color:"#529a00ff" , background:"white" , border:"solid #529a00ff 3px"}}  >
+            <CheckIcon  />
+          </IconButton>
+            <IconButton aria-label="check" style={{color:"#3f3cffff" , background:"white" , border:"solid #3f3cffff 3px"}}  >
+            <ModeEditOutlineOutlinedIcon  />
+          </IconButton>
+            <IconButton aria-label="check" style={{color:"#b30000ff" , background:"white" , border:"solid #b30000ff 3px"}}  >
+            <DeleteOutlineOutlinedIcon  />
+          </IconButton>
+          </Grid>
+          {/* === Actions Buttons === */}
+      </Grid>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
     </>);
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,8 +3,6 @@ import Container from '@mui/material/Container';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
@@ -15,7 +13,7 @@ import ToDo from './ToDo';
 
 export default function ToDoList() {
   return (
-      <Container maxWidth="sm">
+      <Container maxWidth="md">
         <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography variant='h2'>
@@ -47,9 +45,6 @@ export default function ToDoList() {
           {/* === ALL ToDos === */}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
       </Container>
   );
