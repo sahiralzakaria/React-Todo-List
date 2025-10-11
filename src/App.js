8d3630e2d50256a42d3be5f040928901c3dda721
sahir zakaria <sahir.alzakaria@gmail.com>
2025-10-11 15:59:07 +0300

    fonts update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import './App.css';
-import ToDoList from './components/ToDoList';
+import "./App.css";
+import ToDoList from "./components/ToDoList";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Alexandria",
+  },
+});
 
 function App() {
   return (
-    <div className="App" style={{
-
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center",
-      height:"100vh",
-      background: "#1e1e2f",
-      direction:"rtl"
-
-    }}>
-      <ToDoList/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          background: "#1e1e2f",
+          direction: "rtl",
+        }}
+      >
+        <ToDoList />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,38 +1,81 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/GridLegacy';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/GridLegacy";
 
 //Icons
-import IconButton from '@mui/material/IconButton';
-import CheckIcon from '@mui/icons-material/Check';
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import IconButton from "@mui/material/IconButton";
+import CheckIcon from "@mui/icons-material/Check";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 
-
-export default function ToDo(){
-    return (<>
-    <Card sx={{ minWidth: 275 , background:"#0f1963ff" , color:"white" , marginTop:5 }}>
-      <CardContent >
-        <Grid container spacing={2} style={{padding:0 ,  margin:0 }} >
-          <Grid item xs={8} >
-              <Typography variant='h5' style={{textAlign:"right"}}>المهمة الأولى</Typography>
+export default function ToDo() {
+  return (
+    <>
+      <Card
+        className="todoCard"
+        sx={{
+          minWidth: 275,
+          background: "#0f1963ff",
+          color: "white",
+          marginTop: 5,
+        }}
+      >
+        <CardContent>
+          <Grid container spacing={2} style={{ padding: 0, margin: 0 }}>
+            <Grid item xs={8}>
+              <Typography variant="h5" style={{ textAlign: "right" }}>
+                المهمة الأولى
+              </Typography>
+              <Typography variant="h6" style={{ textAlign: "right" }}>
+                التفاصيل الخاصة بالمهمة الأولى{" "}
+              </Typography>
+            </Grid>
+            {/* Actions Buttons */}
+            <Grid
+              xs={4}
+              display="flex"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <IconButton
+                className="iconButton"
+                aria-label="check"
+                style={{
+                  color: "#529a00ff",
+                  background: "white",
+                  border: "solid #529a00ff 3px",
+                }}
+              >
+                <CheckIcon />
+              </IconButton>
+              <IconButton
+                className="iconButton"
+                aria-label="check"
+                style={{
+                  color: "#3f3cffff",
+                  background: "white",
+                  border: "solid #3f3cffff 3px",
+                }}
+              >
+                <ModeEditOutlineOutlinedIcon />
+              </IconButton>
+              <IconButton
+                className="iconButton"
+                aria-label="check"
+                style={{
+                  color: "#b30000ff",
+                  background: "white",
+                  border: "solid #b30000ff 3px",
+                }}
+              >
+                <DeleteOutlineOutlinedIcon />
+              </IconButton>
+            </Grid>
+            {/* === Actions Buttons === */}
           </Grid>
-          {/* Actions Buttons */}
-          <Grid  xs={4} display="flex" justifyContent="space-around" alignItems="center"> 
-            <IconButton aria-label="check" style={{color:"#529a00ff" , background:"white" , border:"solid #529a00ff 3px"}}  >
-            <CheckIcon  />
-          </IconButton>
-            <IconButton aria-label="check" style={{color:"#3f3cffff" , background:"white" , border:"solid #3f3cffff 3px"}}  >
-            <ModeEditOutlineOutlinedIcon  />
-          </IconButton>
-            <IconButton aria-label="check" style={{color:"#b30000ff" , background:"white" , border:"solid #b30000ff 3px"}}  >
-            <DeleteOutlineOutlinedIcon  />
-          </IconButton>
-          </Grid>
-          {/* === Actions Buttons === */}
-      </Grid>
-      </CardContent>
-    </Card>
-    </>);
+        </CardContent>
+      </Card>
+    </>
+  );
 }
